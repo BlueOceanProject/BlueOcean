e14dbadc8e4d88c91ab7a56092fd4fbd263ba208
rachel <jones.rachel.a464@gmail.com>
2021-08-28 15:03:23 -0700

    install react share and create user profile component

---
+++ client/src/UserProfile/UserProfile.jsx
@@ -0,0 +1,13 @@
+import React { useState } from 'react';
+
+const UserProfile  = () => {
+  const [myProfile, setMyProfile] = useState(false);
+
+  render (
+    return (
+      <h2>Profile Page</h2>
+    )
+  )
+}
+
+export default UserProfile;
