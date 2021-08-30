330c4bd245b7145a9318d537d4fc219b701bbdec
lipingh <lipingh@andrew.cmu.edu>
2021-08-30 13:54:05 -0700

    test on more dummy data

--- database/controllers/feeds.js
+++ database/controllers/feeds.js
@@ -1,13 +1,10 @@
 const Feeds = require('../models/feeds');
 
 const getLatestFeedsByUser = (params, callback) => {
-  const perPage = 5;
+  const perPage = 3;
   const { pageNum, userName } = params;
-  console.log({ pageNum, userName });
   const offset = (pageNum - 1) * perPage;
-  // console.log(offset);
   const re = userName ? new RegExp(userName, 'i') : /.*.*/;
-  // console.log(re);
   Feeds.find({
     "userName": re
   })
@@ -15,7 +12,6 @@ const getLatestFeedsByUser = (params, callback) => {
       if (err) {
         callback(err);
       } else {
-        // console.log('docs', docs);
         callback(null, docs);
       }
     })
