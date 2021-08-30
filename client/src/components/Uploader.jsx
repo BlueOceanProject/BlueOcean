ed3ba3db23d2a86e8a223d6a9da093b4f4a78e05
Jessica Hu <ruiruihu001@gmail.com>
2021-08-30 11:12:27 -0700

    s3

---
+++ client/src/components/Uploader.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const Uploader = (props) => {
+  const fileInput = useRef(null);
+
+  const handleFileInput = (e) => {
+    // handle validations
+    const file = e.target.files[0];
+
+    if (file.size > 1024*1024) {
+      props.onFileSelectError({ error: "File size cannot exceed more than 1MB" });
+    } else {
+      props.onFileSelectSuccess(file);
+    }
+  }
+
+  return (
+    <div className="file-uploader">
+      <input type="file" onChange={handleFileInput} />
+      {/* <button onClick={e => fileInput.current && fileInput.current.click()} className="btn btn-primary" /> */}
+    </div>
+  );
+}
+
+export default Uploader;
