d186bcff73184c5669bca522e6238afaeb96d3d0
lipingh <lipingh@andrew.cmu.edu>
2021-08-30 14:24:03 -0700

    remove unnecceary const url

--- client/src/components/HomePage/hooks/useSearchFeeds.jsx
+++ client/src/components/HomePage/hooks/useSearchFeeds.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-const url = '';
+
 function useSearchFeeds(query, pageNum) {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(false);
