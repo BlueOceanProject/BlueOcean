bbbc1fe2e1f1da92a8d3ab128fca392568e68dad
VinnieGlaser <glaservinnie@gmail.com>
2021-08-30 12:11:02 -0700

    audio upload implemented

--- client/src/components/Workstation/Import.jsx
+++ client/src/components/Workstation/Import.jsx
@@ -6,20 +6,23 @@ const Import = () => {
   const [file, setFile] = useState('');
 
   const uploadChange = (event) => {
-    setFile(event.target.files[0])
+    event.target.files[0].arrayBuffer().then((data) => {
+      const blob = new Blob([data], { type: 'audio/wav'});
+      setFile(window.URL.createObjectURL(blob));
+    });
   }
 
   const submitFile = (event) => {
     event.preventDefault();
-
   }
 
   return (
     <div className="import">
-      <form onSubmit={submitFile}>
+
         <input id="fileItem" type="file" title="Upload" onChange={(e) => (uploadChange(e))}/>
+        <audio controls src={file} />
         <button type="submit">Submit</button>
-      </form>
+
       {/* <button onClick={playHandler}>Play</button> */}
     </div>
   )
