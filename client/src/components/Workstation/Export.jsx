bfc8f9544dfbc3801ace2acbcb91655e63e0625b
Jessica Hu <ruiruihu001@gmail.com>
2021-08-30 23:58:21 -0700

    database

--- client/src/components/Workstation/Export.jsx
+++ client/src/components/Workstation/Export.jsx
@@ -3,36 +3,19 @@ import axios from 'axios';
 
 const Export = (props) => {
   // console.log(props.uploadAudio && props.uploadAudio.src);
+  const [songName, setSongName] = useState('');
+
   const exportSong = () => {
-    // console.log(props.uploadAudio.src)
-    // axios.get(props.uploadAudio.src)
-    //   .then(res => {
-    //     console.log('ehwfiwefw', res);
-    //     return res.data
-    //   })
-    //   .then(blob => {
-    //     const formData = new FormData();
-    //     // formData.append("name", 'sdfsdfsdfsdf' + Date.now());
-    //     // formData.append("file", blob);
-    //     var file = new File([blob], ".wav")
-    //     formData.append("name", 'sdfsdfsdfsdf' + Date.now());
-    //     formData.append("file", file)
-    //     return axios.post('http://localhost:3000/upload', formData)
-    //   })
-    //   .then((res) => {
-    //     alert("File Upload success");
-    //   })
-    //   .catch((err) => alert("File Upload Error"));
     fetch(props.uploadAudio.src)
       .then(response => {
         // console.log(response);
-        return response.blob()
+        return response.blob();
       })
       .then(blob => {
         // console.log(blob);
         const formData = new FormData();
         var file = new File([blob], "dummy.mp3")
-        formData.append("name", 'sdfsdfsdfsdf' + Date.now());
+        formData.append("name", songName);
         formData.append("file", file)
         return axios.post('http://localhost:3000/upload', formData)
       })
@@ -46,6 +29,11 @@ const Export = (props) => {
 
   return (
     <div>
+        <form>
+          <label>Song name:
+            <input type="text" name="name" onChange={(e) => setSongName(e.target.value)}/>
+          </label>
+        </form>
         <button onClick={e => exportSong(e)}>Export Song</button>
     </div>
   )
--- database/controllers/users.js
+++ database/controllers/users.js
@@ -5,6 +5,25 @@ const postSignUpUser = (userInfo) => {
   return user.save();
 };
 
-module.exports = {
-  postSignUpUser,
-};
+const insertSongForUser = (userName, url, songName) => {
+  // console.log('insertSongForUser');
+  const now = new Date()
+  const song = {
+    userName: userName,
+    songName: songName,
+    url: url,
+    published: false,
+    createdAt: now
+  }
+
+  Users.update(
+    { userName: userName }, { $push: { songs: song } }, (error, success) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(success);
+      }
+    });
+}
+
+module.exports = { insertSongForUser, postSignUpUser };
