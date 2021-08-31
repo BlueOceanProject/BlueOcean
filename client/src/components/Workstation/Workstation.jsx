db8fc49717e629981734640e4890bfe8d043e531
nickhanrahan <nicholashanr@gmail.com>
2021-08-31 16:22:53 -0700

    comment out crunker

--- client/src/components/Workstation/Workstation.jsx
+++ client/src/components/Workstation/Workstation.jsx
@@ -6,7 +6,7 @@ import Import from './Import.jsx';
 import Export from './Export.jsx';
 import css from './workstation.css';
 
-const crunker = new Crunker({sampleRate: 48000});
+// const crunker = new Crunker({sampleRate: 48000});
 
 const Workstation = (props) => {
   const [recordState, setRecordState] = useState(null);
