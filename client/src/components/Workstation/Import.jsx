82381b152168555920e9c228e160ce0dd8050ff7
nickhanrahan <nicholashanr@gmail.com>
2021-08-31 10:51:29 -0700

    undo import button

--- client/src/components/Workstation/Import.jsx
+++ client/src/components/Workstation/Import.jsx
@@ -3,30 +3,19 @@ import axios from 'axios';
 
 const Import = ({ setUploadAudio, setAudioFile, audioFile }) => {
 
-  const [audioURL, setAudioURL] = useState('');
-  const [audioObj, setAudioObj] = useState({});
-
   const uploadChange = (event) => {
     event.target.files[0].arrayBuffer().then((data) => {
       const blob = new Blob([data], { type: 'audio/wav'});
       const url = window.URL.createObjectURL(blob);
-      setAudioURL(url);
-      setAudioObj(new Audio(url))
+      setAudioFile(url);
+      setUploadAudio(new Audio(url))
     });
-  }
 
-  const importSubmit = (event) => {
-    event.preventDefault();
-    setAudioFile(audioURL);
-    setUploadAudio(audioObj)
   }
 
   return (
     <div className="import">
-      <form className="import-form" onSubmit={importSubmit}>
-        <input id="fileItem" type="file" title="Upload" onChange={(e) => (uploadChange(e))}/>
-        <button type="submit">Import</button>
-      </form>
+      <input id="fileItem" type="file" title="Upload" onChange={(e) => (uploadChange(e))}/>
       <audio controls src={audioFile} />
     </div>
   )
