bb4b40cbba3cabdeb1d0017ad1b5ea330c8136dd
Preston Carroll <prestondcarroll@gmail.com>
2021-08-30 18:01:17 -0700

    toolbar saving

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,13 +2,22 @@ import React, { useState, useEffect } from 'react';
 import Feed from './HomePage/Feed.jsx'
 import Workstation from './Workstation/Workstation.jsx';
 import Viewer from './Viewer.jsx';
+import Toolbar from './Toolbar.jsx';
+import {BrowserRouter as Router , Route, Switch } from 'react-router-dom';
 
 const App = () => {
 
   return (
-    <div>
-      <h1>Blue Ocean</h1>
-    </div>
+    <Router>
+      <div>
+        <Toolbar />
+        <Switch>
+          <Route path="/">
+            <h1>Blue Ocean</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
