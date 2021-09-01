261b87d15e9e905e5b751f5c5404281da332937d
rachel <jones.rachel.a464@gmail.com>
2021-09-01 10:05:58 -0700

    fix conditional rendering, ass sign in sign up messages

--- client/src/components/UserProfile/SongListItem.jsx
+++ client/src/components/UserProfile/SongListItem.jsx
@@ -14,7 +14,7 @@ import {
 } from "react-share";
 
 
-const SongListItem = ({ myProfile, song, userImg}) => {
+const SongListItem = ({ myProfile, song, userImg, signedOut}) => {
   const handleAddToFeed = () => {
     song.profileImg = userImg
     axios.post('/feeds', song)
@@ -26,46 +26,47 @@ const SongListItem = ({ myProfile, song, userImg}) => {
     })
   }
   return (
-  <div>
-  <div className="eachSong">
-    <span>{song.songName}</span>
-    <audio
-        controls
-        src={song.url}
-      >
-        Your browser does not support the
-        <code>audio</code> element.
-      </audio>
-      {myProfile && !song.published ? <Button variant="outline-dark" onClick={handleAddToFeed}>Publish</Button> : null}
-  </div>
-  <FacebookShareButton
-  url="https://www.google.com"
-  quote={"http://cd.textfiles.com/10000gp2/500SNDS/8_38.WAV"}
-  hashtag="#Harmony">
-    <FacebookIcon size={36} round/>
-  </FacebookShareButton>
-  <TwitterShareButton
-  url="https://www.google.com"
-  quote={"http://cd.textfiles.com/10000gp2/500SNDS/8_38.WAV"}
-  hashtag="#Harmony">
-  <TwitterIcon size={36} round/>
-  </TwitterShareButton>
-  <RedditShareButton
-  title="song url?"
-  url="https://www.google.com"
-  quote={"http://cd.textfiles.com/10000gp2/500SNDS/8_38.WAV"}
-  >
-    <RedditIcon size={36} round/>
-  </RedditShareButton>
-  <TumblrShareButton
-  title="Checkout this song I made on Harmony!"
-  url="https://www.google.com"
-  caption={"http://cd.textfiles.com/10000gp2/500SNDS/8_38.WAV"}
-  tags={["#Harmony"]}
-  >
-    <TumblrIcon size={36} round/>
-  </TumblrShareButton>
-  </div>
-  )
-}
+        <div>
+        <div className="eachSong">
+          <span>{song.songName}</span>
+          <audio
+              controls
+              controlsList={signedOut ? "nodownload" : ""}
+              src={song.url}
+            >
+              Your browser does not support the
+              <code>audio</code> element.
+            </audio>
+            {myProfile && !song.published ? <Button variant="outline-dark" onClick={handleAddToFeed}>Publish</Button> : null}
+        </div>
+        <FacebookShareButton
+        url="https://www.google.com"
+        quote={song.url}
+        hashtag="#Harmony">
+          <FacebookIcon size={36} round/>
+        </FacebookShareButton>
+        <TwitterShareButton
+        url="https://www.google.com"
+        quote={song.url}
+        hashtag="#Harmony">
+        <TwitterIcon size={36} round/>
+        </TwitterShareButton>
+        <RedditShareButton
+        title={song.songName}
+        url="https://www.google.com"
+        quote={song.url}
+        >
+          <RedditIcon size={36} round/>
+        </RedditShareButton>
+        <TumblrShareButton
+        title={song.songName}
+        url="https://www.google.com"
+        caption={song.url}
+        tags={["#Harmony"]}
+        >
+          <TumblrIcon size={36} round/>
+        </TumblrShareButton>
+        </div>
+  );
+};
 export default SongListItem;
