93dfc8e9bc4670e4a1ce59a9774e31b0fec37c31
Jiaqian Zhu <blairzhu19@gmail.com>
2021-09-01 10:57:11 -0700

    update username in global state as auth state changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useReducer } from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { AuthProvider } from './Authentication/AuthContext.js';
 import SignUp from './Authentication/SignUp.jsx';
@@ -8,6 +9,7 @@ import Home from './Home.jsx';
 const initialState = {
   userId: '',
   query: '',
+  username: '',
 };
 
 const reducer = (state, action) => {
@@ -16,6 +18,8 @@ const reducer = (state, action) => {
       return { ...state, userId: action.data}
     case 'updateQuery':
       return { ...state, query: action.data}
+    case 'updateUsername':
+      return { ...state, username: action.data}
     default:
       return state;
   }
@@ -25,6 +29,20 @@ export const GlobalContext = React.createContext();
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    if (state.userId) {
+      axios.get(`/username/${state.userId}`)
+        .then((res) => {
+          dispatch({ type: 'updateUsername', data: res });
+        })
+        .catch((err) => {
+          console.error(err);
+        })
+    } else {
+      dispatch({ type: 'updateUsername', data: '' });
+    }
+  }, [state.userId]);
 
   return (
     <div>
