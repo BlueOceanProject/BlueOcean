117dab0687b343c42339256b011b9f88977d62d6
Preston Carroll <prestondcarroll@gmail.com>
2021-09-01 17:06:59 -0700

    Delete test.js

--- database/controllers/test.js
+++
@@ -1,9 +0,0 @@
-const Users = require('../models/users');
-
-Users.findOneAndUpdate({
-  userName: song.userName,
-  createdAt: song.createdAt,
-}, {published: true})
-
-
-db.users.findOneAndUpdate({songs._id: "0a1567d1-2bbd-4dc2-9ffe-1f4361cc99c6"}, {$set: {songs.$.published: true}})
